f3f67a258e71718bb4a84ee46924fe93122b8fc7
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-06-15 23:48:52 +0200

    Added player, sprites class, move module

--- src/states/Main.js
+++ src/states/Main.js
@@ -1,21 +1,43 @@
-import ExampleObject from 'objects/ExampleObject';
+import Sprites from 'objects/Sprites';
+import move from 'modules/move';
+
+var duck;
+var game;
+var platforms;
+var cursors;
+var sprites;
+var ground;
 
 class Main extends Phaser.State {
 
 	create() {
+		game = this.game;
+		sprites = new Sprites(game);
 
 		//Enable Arcade Physics
-		this.game.physics.startSystem(Phaser.Physics.ARCADE);
-
+		game.physics.startSystem(Phaser.Physics.ARCADE);
 		//Set the games background colour
-		this.game.stage.backgroundColor = '#cecece';
+		game.stage.backgroundColor = '#fff';
 
-		//Example of including an object
-		//let exampleObject = new ExampleObject(this.game);
+		platforms = game.add.group();
+		platforms.enableBody = true;
+		ground = platforms.create(0, game.world.height - 80);
+		ground.body.immovable = true;
+
+		duck = sprites.duck();
+		game.physics.arcade.enable(duck);
+
+		duck.body.bounce.y = 0.2;
+		duck.body.gravity.y = 300;
+		duck.body.collideWorldBounds = true;
+
+		cursors = game.input.keyboard.createCursorKeys();
+
 	}
 
 	update() {
-		
+		game.physics.arcade.collide(duck, platforms);
+		move(duck, cursors);
 	}
 
 }
