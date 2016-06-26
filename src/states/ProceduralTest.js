53f2bb3f578425ccc22de51d67509e2468bcc7cb
Jonatan Dahl <dahl.jonatan@gmail.com>
2016-06-26 16:52:01 +0200

    dynamic tilemap. wrong orientation though

--- src/states/ProceduralTest.js
+++ src/states/ProceduralTest.js
@@ -23,41 +23,76 @@ class ProceduralTest extends Phaser.State {
 
     let p = new Processing();
 
-    let pixelWidth = 8;
+    let pixelWidth = 32;
     let pixelHeight = pixelWidth;
 
-    // a block should be 32 pixels in size (4 sprite pixels..)
-    // add spacing of 1 to easier see individual blocks
-    let blockSize = pixelWidth*4+1;
-
-    graphics.moveTo(0, 0);
-    graphics.lineStyle(1, 0x0000FF, 0.8);
+    // a tile should be 32 pixels in size (4 sprite pixels..)
+    // add spacing of 1 to easier see individual tiles
+    let tileSize = pixelWidth;
 
     let scale = 0.0001;
     p.noiseSeed(1);
     p.noiseDetail(8, 0.7);
 
-    // go throught the entire viewport, increment by blocksize
-    for (let screenX = 0; screenX < this.game.camera.width; screenX += blockSize) {
+    // for debug line
+    graphics.moveTo(0, 0);
+    graphics.lineStyle(1, 0x0000FF, 0.8);
+
+    // GENERATE MAP DATA
+    // go throught the entire viewport, increment by tilesize
+    let mapData = '';
+    for (let screenX = 0; screenX < this.game.camera.width; screenX += tileSize) {
 
       // get Y value for this screen X coordinate
       let yNoise = p.noise(screenX * scale, 1, 1) * 700;
 
-      for (let screenY = 0; screenY < this.game.camera.height; screenY += blockSize) {
+      for (let screenY = 0; screenY < this.game.camera.height; screenY += tileSize) {
 
-        // render a sprite block if current screen Y coordinate is lower
+        // render a sprite tile if current screen Y coordinate is lower
         // than the y noise value (screen coords Y is inverted, hence the >)
         if (screenY > yNoise) {
-          let data = ['3333','3553','3553','3333'];
-          let id = 'block'+screenX+screenY;
-          this.game.create.texture(id, data, pixelWidth, pixelHeight);
-          this.game.add.sprite(screenX, screenY, id);
+          mapData += '0'; // pick the "first" tile (we'll only have one tile...)
+
+          // let data = ['3333','3553','3553','3333'];
+          // let id = 'tile'+screenX+screenY;
+          // this.game.create.texture(id, data, pixelWidth, pixelHeight);
+          // this.game.add.sprite(screenX, screenY, id);
+        } else {
+          mapData += '.';
+        }
+
+        // add comma except after last item
+        if (screenY < this.game.camera.height - tileSize) {
+          mapData += ',';
         }
       }
 
-      // debug print the line
+      // add new line except after last line
+      if (screenX < this.game.camera.width - tileSize) {
+        mapData += '\n';
+      }
+
+      // print debug line
       graphics.lineTo(screenX, yNoise)
     }
+
+    console.log(mapData)
+
+    // ref: http://phaser.io/examples/v2/category/tilemaps
+
+    // Add data to the cache
+    this.game.cache.addTilemap('bgTiles', null, mapData, Phaser.Tilemap.CSV);
+    let map = this.game.add.tilemap('bgTiles', tileSize, tileSize);
+    // let tileWidth = Math.floor(this.game.camera.width / tileSize);
+    // let tileHeight = Math.floor(this.game.camera.width / tileSize);
+
+    //  'tiles' = cache image key,
+    this.game.create.texture('tile', ['5'], tileSize, tileSize);
+    map.addTilesetImage('tile', 'tile', tileSize, tileSize);
+    let bgLayer = map.createLayer(0);
+
+    bgLayer.resizeWorld();
+
   }
 
   update() {
